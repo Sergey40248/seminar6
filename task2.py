7e0a60d4890700e8ecdbf6f9a5ab3aa4a77bc84c
Sergey <Sergey40248@mail.ru>
2022-12-26 14:52:32 +0300

    modified task2

--- task2.py
+++ task2.py
@@ -3,12 +3,10 @@
 # Все задачи решать с помощью использования 
 # лямбд, filter, map, zip, enumerate, 
 
-# a = [12,'sadf',5643]
-# stro = list(filter(lambda x: isinstance(x, str),a))
 
-# print(stro,)
 
 a = [12,'sadf',5643]
 s = list(filter(lambda x: isinstance(x, str),a))
 i = list(filter(lambda x: isinstance(x, int),a))
+ 
 print(f'{s}, {i}')
